37efaa964f3969b1d01ee945bfca0a9db5413fd3
Shubham-Vetal <163741335+Shubham-Vetal@users.noreply.github.com>
2025-10-28 22:34:27 +0530

    Rename import from purchaseCourse.controller to coursePurchase.controller

--- Backend/routes/purchaseCourse.route.js
+++ Backend/routes/purchaseCourse.route.js
@@ -5,7 +5,7 @@ import {
   getAllPurchasedCourse,
   getCourseDetailWithPurchaseStatus,
   stripeWebhook,
-} from "../controllers/purchaseCourse.controller.js";
+} } from "../controllers/coursePurchase.controller.js";
 
 
 const router = express.Router();
