f6449f656543ace4b6154c89f6f1ae7aaa999a0b
Shubham-Vetal <163741335+Shubham-Vetal@users.noreply.github.com>
2025-10-29 12:08:13 +0530

    Enhance error handling in isAuthenticated middleware
    Added return statement in error handling for token verification.

--- Backend/middlewares/isAuthenticated.js
+++ Backend/middlewares/isAuthenticated.js
@@ -3,23 +3,33 @@ import jwt from "jsonwebtoken";
 const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
+    
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
         success: false,
       });
     }
+    
     const decode = await jwt.verify(token, process.env.JWT_SECRET);
+    
     if (!decode) {
       return res.status(401).json({
         message: "Invalid token",
         success: false,
       });
     }
+    
     req.id = decode.userId;
     next();
   } catch (error) {
     console.log(error);
+    //  Add return statement 
+    return res.status(401).json({
+      message: "Invalid or expired token",
+      success: false,
+    });
   }
 };
+
 export default isAuthenticated;
